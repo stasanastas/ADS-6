cb56f79f3a523606900817d67812291e78145f38
Анастасия Карова <76094090+stasanastas@users.noreply.github.com>
2021-06-04 01:24:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,30 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> tree;
+  std::ifstream file(filename);
+  int capToLow = 'a' - 'A';
+  std::string word = "";
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+    return tree;
+  }
+  while(!file.eof()) {
+    char ch = file.get();
+    if (ch >= 'A' && ch <= 'Z')
+      ch = capToLow;
+    boolean inWord = false;
+    if (ch >= 'a' && ch <= 'z' && inWord == false) {
+      inWord = true;
+      word += ch;
+    } else if (ch >= 'a' && ch <= 'z' && inWord == true) {
+      word += ch;
+    } else if (!(ch >= 'a' && ch <= 'z') && inWord == true) {
+      inWord = false;
+      tree.add(word);
+      word = "";
+    }
+  }
+  file.close();
+  return tree;
 }
